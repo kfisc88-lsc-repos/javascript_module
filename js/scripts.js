9583e8a2054c591882bbf7a0e12a3469cda57862
krfisc <krfisc@outlook.com>
2019-11-20 02:02:12 -0600

    Simplified js for processOrder formatting changes to html and adjust css

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,20 @@ File: scripts.js
 
 */
 
-processOrder();
-
 
 function processOrder() {
-  var cost;
-  var 
-  console.log(ordered)
+  var cost = 0.00;
+  var ordered = ["burger", "dblburger", "chicken", "vegburger", "fish"]
+  var i;
+
+  for (i = 0; i < ordered.length; i++) {
+      if (document.getElementById(ordered[i]).checked == true) {
+          cost += parseFloat(document.getElementById(ordered[i]).value);
+      }
+  }
+  console.log(cost);
+  document.getElementById("total").innerHTML = "Your total is: $" + cost;
 }
+
+
+if (document.getElementById())
