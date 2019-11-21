5c8cf21b22ce21615880a76f17117ce1449b9f3a
krfisc <krfisc@outlook.com>
2019-11-21 00:44:58 -0600

    Fixed table display for "Add Rows" button, wrote js for the function

--- js/scripts.js
+++ js/scripts.js
@@ -23,4 +23,40 @@ function processOrder() {
 }
 
 
-if (document.getElementById())
+function getCustInfo() {
+        document.getElementById("deliveryAddr").style.display = "block";
+}
+
+
+function hideCustInfo() {
+        document.getElementById("deliveryAddr").style.display = "none";
+}
+
+function showOrderTable() {
+        document.getElementById("order_table").style.display = "block";
+}
+
+function generateRows() {
+    var num_rows = document.getElementById("add_rows").value;
+    var table_rows = "";
+    var i = 0;
+
+    while( i < num_rows ) {
+        table_rows = table_rows +
+        "<tr>" +
+            "<td>" + (i+1) + "</td>" +
+            "<td><select name='item[]'>" +
+                    "<option value='0'></option>" +
+                    "<option value='8.99'>Classic Rock Burger $8.99</option>" +
+                    "<option value='11.99'>Double Rock Burger $11.99</option>" +
+                    "<option value='10.99'>Fried Chicken Sandwich $10.99</option>" +
+                    "<option value='8.99'>Portobella Burger $8.99</option>" +
+                    "<option value='11.99'>Smoked Fish Sandwich $11.99</option>" +
+                "</select></td>" +
+            "<td><input type='text' name='name[]' /></td>" +
+        "</tr>";
+        i = i+1;
+    }
+
+    document.getElementById("tablebody").innerHTML = table_rows;
+}
