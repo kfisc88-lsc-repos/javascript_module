558263f20b185240af5e69df833a4c3f9e2752ed
krfisc <krfisc@outlook.com>
2019-11-21 01:58:18 -0600

    Added images to assets, comments to js

--- js/scripts.js
+++ js/scripts.js
@@ -8,39 +8,46 @@ File: scripts.js
 */
 
 
+/* Calculates the total cost for selected items on the menu */
 function processOrder() {
-  var cost = 0.00;
-  var ordered = ["burger", "dblburger", "chicken", "vegburger", "fish"]
-  var i;
+    /* Initialize variables for list of IDs, cost, and i*/
+    var cost = 0.00;
+    var ordered = ["burger", "dblburger", "chicken", "vegburger", "fish"]
+    var i;
 
-  for (i = 0; i < ordered.length; i++) {
-      if (document.getElementById(ordered[i]).checked == true) {
-          cost += parseFloat(document.getElementById(ordered[i]).value);
-      }
-  }
-  console.log(cost);
-  document.getElementById("total").innerHTML = "Your total is: $" + cost;
+    /* Iterates through each item of ordered, adding checked items to cost */
+    for (i = 0; i < ordered.length; i++) {
+        if (document.getElementById(ordered[i]).checked == true) {
+            cost += parseFloat(document.getElementById(ordered[i]).value);
+        }
+    }
+    /* Returns the total cost */
+    document.getElementById("total").innerHTML = "Your total is: $" + cost;
 }
 
-
+/* Displays the Delivery Address form if delivery is selected */
 function getCustInfo() {
         document.getElementById("deliveryAddr").style.display = "block";
 }
 
-
+/* Hides the Delivery Address form if carryout or nothing is selected */
 function hideCustInfo() {
         document.getElementById("deliveryAddr").style.display = "none";
 }
 
+/* Displays the Order Table when rows are added to it */
 function showOrderTable() {
         document.getElementById("order_table").style.display = "block";
 }
 
+/* Adds table rows with drop-down menus */
 function generateRows() {
+    /* Gets number of rows to create */
     var num_rows = document.getElementById("add_rows").value;
     var table_rows = "";
     var i = 0;
 
+    /* Numbers each table row, adds drop down for menu, and field for name */
     while( i < num_rows ) {
         table_rows = table_rows +
         "<tr>" +
@@ -58,5 +65,6 @@ function generateRows() {
         i = i+1;
     }
 
+    /* Returns the table rows to the document */
     document.getElementById("tablebody").innerHTML = table_rows;
 }
